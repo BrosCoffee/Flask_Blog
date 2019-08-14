31af4382df3aea3074e5682d2cd1787aaf569270
Raymond <raymondcyang0219@gmail.com>
2019-08-14 20:33:42 +0800

    templates file created and README.md 1st edited

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 &nbsp; The project is using Python and the [Flask framework](https://flask.palletsprojects.com/en/1.1.x/) to create a personal blog.
 
 ### Details
-> * flaskblog.py: @app.route('/')
+> * flaskblog.py: 
+>> * `@app.route('/')`
+>> * `app = Flask(__name__)`
+>> * `@app.route('/')`<br>`def home():`
+>> * `if __name__ == '__main__':`<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`app.run(debug=True)`
+> * home.html
+>> * `from flask import Flask, render_template`
+>> * `{% ... %}` for python syntax
+>> * `{{ ... }}` for variables
 
 ##### Learning resource: Corey Schafer https://www.youtube.com/channel/UCCezIgC97PvUuR4_gbFUs5g
-###### (Last updated date: Aug. 13th 2019) 
+###### (Last updated date: Aug. 14th 2019) 
--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Raymond Yang',
+        'title': 'Blog post #1',
+        'content': 'My first blog post.',
+        'date_posted': 'Aug. 14th 2019'
+    },
+    {
+        'author': 'Meliza Anzures',
+        'title': 'Blog post #2',
+        'content': 'I am the princess.',
+        'date_posted': 'Aug. 14th 2019'
+    }
+]
+
 @app.route('/')
 def home():
-    return '<h1>Home Page</h1> '
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1> '
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
