902e0b136e3e3328d64e1572d78c1ced2a89cac3
Raymond <raymondcyang0219@gmail.com>
2019-08-15 11:58:02 +0800

    POST, GET, alerts, and validations added

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -28,14 +28,23 @@ def home():
 def about():
     return render_template('about.html', title = "Ray's blog - About")
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title = "Ray's blog - Register", form = form)
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'raymondcyang0219@gmail.com' and form.password.data == 'Passw@rd':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title = "Ray's blog - Login", form = form)
 
 if __name__ == '__main__':
