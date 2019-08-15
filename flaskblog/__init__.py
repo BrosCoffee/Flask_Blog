1b45a59ace397f48a2517f8785543e60a347f608
Raymond <raymondcyang0219@gmail.com>
2019-08-15 23:15:29 +0800

    routes.py hashed_password added, and db connected

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd0c948886a8a19047c3255db3670afe4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
